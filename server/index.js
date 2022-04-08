d2c960550ce65e642f1c5c8978a99213fb685a76
Harsh Singh <harshsingh16d@gmail.com>
2022-04-08 21:58:49 +0530

    gitignore

--- server/index.js
+++ server/index.js
@@ -2,26 +2,28 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import postsRouter from './routes/posts.js';
 import usersRouter from './routes/users.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('THIS IS THE API WEBPAGE');
+    res.send('THIS IS THE BUCKET LIST API WEBPAGE');
 });
 app.use('/posts', postsRouter);
 app.use('/user', usersRouter);
 
-const CONNECTION = 'mongodb+srv://HarshS1611:gaamabanta@cluster0.c4jr0.mongodb.net/bucketlist?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
